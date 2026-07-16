a5152b16559872a19941b5d25ee7585cbc31bcf4
syriable <info@syriable.com>
2026-07-16 17:53:26 +0200

    fix: document LooseString model properties for PHPStan

--- src/Models/LooseString.php
+++ src/Models/LooseString.php
@@ -4,8 +4,24 @@ namespace Syriable\Translations\Models;
 
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Support\Carbon;
 use Syriable\Translations\Enums\LooseStringStatus;
 
+/**
+ * @property int $id
+ * @property string $file_path
+ * @property int|null $line
+ * @property string $text
+ * @property string $text_hash
+ * @property string|null $element_type
+ * @property string|null $scanner
+ * @property LooseStringStatus $status
+ * @property int|null $phrase_id
+ * @property array<array-key, mixed>|null $placeholders
+ * @property Carbon|null $created_at
+ * @property Carbon|null $updated_at
+ * @property-read Phrase|null $phrase
+ */
 class LooseString extends TranslationModel
 {
     protected string $table_ = 'loose_strings';
